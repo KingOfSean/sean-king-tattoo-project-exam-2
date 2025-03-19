722a54a2a3f5e768f4bcc5f9b9db7d9162e12758
SkingAmeriPro <129881621+SkingAmeriPro@users.noreply.github.com>
2025-03-19 08:08:02 -0500

    fixed header margin

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,9 +2,9 @@
 </script>
 
 <div class="flex">
-	<div class="flex h-[40%] w-full items-center bg-[#557DBE0D] pl-8">
+	<div class="flex h-[40%] w-full items-center shadow-[inset_0_0_0_1000px_#557DBE0D] pl-8">
 		<div>
-			<p id="headerText" class="mb-[1rem] mr-[2rem] text-[3.5rem] leading-none font-light">
+			<p id="headerText" class="mb-[2rem] mr-[2rem] mt-[2rem] text-[3.5rem] leading-none font-light">
 				<span class="font-bold text-sky-600">Featured</span>
 				Locations
 			</p>
